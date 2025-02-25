b03d953a5ca879b6c45e1c038d3d764f45b21691
DafeiQiu123 <138055520+DafeiQiu123@users.noreply.github.com>
2025-02-25 16:34:11 -0500

    minor bug fixed with proxy

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ execution_num = 2*60*60//question_refresh_time
 Brown_University_IP = "128.148.204.163"
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument(f'--proxy-server={Brown_University_IP}')
+# chrome_options.add_argument(f'--proxy-server={Brown_University_IP}')
 service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options = chrome_options)
-# driver = webdriver.Chrome(service = service)
+# driver = webdriver.Chrome(service=service, options = chrome_options)
+driver = webdriver.Chrome(service = service)
 
 try:
     # Open Top Hat login page
